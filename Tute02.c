9873c01fe7c3962f61f171ee2f73749aa6a32902
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2022-02-08 15:06:15 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,20 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  float distance ,rent;
+  printf("Enter distance : ");
+  scanf("%f" , &distance);
+  
+  if(distance <= 30 && distance >= 0 )
+    rent = distance * 50;
+  else if(distance > 30)
+    rent = distance * 50 + (distance-30) * 40;
+  else
+  {
+    printf("Invalid distance");
+    return -1;
+  }
+  
+  printf("Rent is  : %.2f ", rent);
   return 0;
 }
